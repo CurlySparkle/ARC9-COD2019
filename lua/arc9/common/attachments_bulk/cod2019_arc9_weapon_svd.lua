950a23a7f78d983861a79285b92b415a4d9de945
Firmeteran <mfirmansyah12345@gmail.com>
2023-02-14 17:46:53 +0700

    Update cod2019_arc9_weapon_svd.lua
    Attachments now has it's own categories.

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_svd.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_svd.lua
@@ -6,13 +6,14 @@ ATT.PrintName = "Short Barrel"
 ATT.CompactName = "Short"
 ATT.Description = [[Short-length barrel. Offers superior handling at the cost of performance.]]
 ATT.SortOrder = 1
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
 
 ATT.Icon = Material("entities/attachs/go_famas_barrel_short.png", "mips smooth")
 ATT.AutoStats = true
 
 ATT.Free = false
 
-ATT.Category = {"go_svd_barrel"}
+ATT.Category = {"cod2019_svd_barrel"}
 ATT.ActivateElements = {"barrel_short"}
 
 ATT.AimDownSightsTimeMult = 0.85
@@ -23,7 +24,7 @@ ATT.RangeMaxMult = 0.85
 ATT.RangeMinMult = 0.85
 ATT.PhysBulletMuzzleVelocityMult = 0.9
 
-ARC9.LoadAttachment(ATT, "csgo_svd_barrel_short")
+ARC9.LoadAttachment(ATT, "cod2019_svd_barrel_short")
 ------------------------------------------------------------------------------------------------
 ATT = {}
 
@@ -34,10 +35,10 @@ ATT.Description = [[Increased ammo capacity to 20 rounds, at the cost of increas
 ATT.Pros = {}
 ATT.Cons = {}
 ATT.SortOrder = 5
-ATT.MenuCategory = "ARC9 - CSGO Attachments"
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
 ATT.Free = false
 
-ATT.Category = {"go_svd_mag"}
+ATT.Category = {"cod2019_svd_mag"}
 ATT.ClipSizeOverride = 20
 ATT.ReloadTimeMult = 1.1
 ATT.AimDownSightsTimeMult = 1.15
@@ -45,4 +46,4 @@ ATT.SprintToFireTimeMult = 1.15
 
 ATT.ActivateElements = {"mag"}
 
-ARC9.LoadAttachment(ATT, "csgo_svd_mag_extended")
+ARC9.LoadAttachment(ATT, "cod2019_svd_mag_extended")
