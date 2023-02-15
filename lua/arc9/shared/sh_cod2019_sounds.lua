036e7277b65c7df92fc012a469a29024f917ed7d
May Lian <twilight_sparkle13@hotmail.com>
2023-02-15 15:42:32 -0300

    Added AS VAL

--- lua/arc9/shared/sh_cod2019_sounds.lua
+++ lua/arc9/shared/sh_cod2019_sounds.lua
@@ -1,4 +1,19 @@
 sound.Add( {
+    name = "COD2019.Asval.Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,110},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/asval/weap_valpha_sup_plr_01.ogg",
+		"weapons/cod2019/asval/weap_valpha_sup_plr_02.ogg",
+		"weapons/cod2019/asval/weap_valpha_sup_plr_03.ogg",
+		"weapons/cod2019/asval/weap_valpha_sup_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
     name = "COD2019.M4A1.Fire",
     channel = 10,
     volume = 1.0,
