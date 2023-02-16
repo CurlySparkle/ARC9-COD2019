99548797e950c29650b974e029158546a6613b5e
May Lian <twilight_sparkle13@hotmail.com>
2023-02-15 21:57:00 -0300

    Added the CR-56 AMAX

--- lua/arc9/shared/sh_cod2019_sounds.lua
+++ lua/arc9/shared/sh_cod2019_sounds.lua
@@ -1,4 +1,34 @@
 sound.Add( {
+    name = "COD2019.cr56amax.Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,110},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/cr56amax/weap_galima_fire_plr_01.ogg",
+		"weapons/cod2019/cr56amax/weap_galima_fire_plr_02.ogg",
+		"weapons/cod2019/cr56amax/weap_galima_fire_plr_03.ogg",
+		"weapons/cod2019/cr56amax/weap_galima_fire_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
+    name = "COD2019.cr56amax.Silenced_Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,110},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/cr56amax/weap_galima_fire_silenced_plr_01.ogg",
+		"weapons/cod2019/cr56amax/weap_galima_fire_silenced_plr_02.ogg",
+		"weapons/cod2019/cr56amax/weap_galima_fire_silenced_plr_03.ogg",
+		"weapons/cod2019/cr56amax/weap_galima_fire_silenced_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
     name = "COD2019.Asval.Fire",
     channel = 10,
     volume = 1.0,
