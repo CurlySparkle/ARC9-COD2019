91aa7bbdc2f36af1f562014da9c21c54974c9aad
May Lian <twilight_sparkle13@hotmail.com>
2023-02-16 21:29:42 -0300

    Added SCAR-17

--- lua/arc9/shared/sh_cod2019.lua
+++ lua/arc9/shared/sh_cod2019.lua
@@ -4,3 +4,12 @@ ARC9.COD2019.BlendEmpty = function(wep)
     local vm = wep:GetOwner():GetViewModel()
     vm:SetPoseParameter("bullets",30-wep:Clip1())
 end
+
+ARC9.COD2019.BlendEmpty2 = function(wep)
+    local vm = wep:GetOwner():GetViewModel()
+    if wep:Clip1() == 0 then
+        vm:SetPoseParameter("empty", 1)
+    else
+        vm:SetPoseParameter("empty", 0)
+    end
+end
--- lua/arc9/shared/sh_cod2019_sounds.lua
+++ lua/arc9/shared/sh_cod2019_sounds.lua
@@ -1,4 +1,34 @@
 sound.Add( {
+    name = "COD2019.Scar.Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,110},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/scar/weap_scharlie_fire_plr_01.ogg",
+		"weapons/cod2019/scar/weap_scharlie_fire_plr_02.ogg",
+		"weapons/cod2019/scar/weap_scharlie_fire_plr_03.ogg",
+		"weapons/cod2019/scar/weap_scharlie_fire_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
+    name = "COD2019.Scar.Silenced_Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,110},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/scar/weap_scharlie_fire_silenced_plr_01.ogg",
+		"weapons/cod2019/scar/weap_scharlie_fire_silenced_plr_02.ogg",
+		"weapons/cod2019/scar/weap_scharlie_fire_silenced_plr_03.ogg",
+		"weapons/cod2019/scar/weap_scharlie_fire_silenced_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
     name = "COD2019.Grau556.Fire",
     channel = 10,
     volume = 1.0,
