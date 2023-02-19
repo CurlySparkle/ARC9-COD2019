5a64f7c6afc30101bc14401bb9b99236e9b31392
May Lian <twilight_sparkle13@hotmail.com>
2023-02-19 18:29:44 -0300

    Added the fennec

--- lua/arc9/shared/sh_cod2019_sounds.lua
+++ lua/arc9/shared/sh_cod2019_sounds.lua
@@ -1,4 +1,34 @@
 sound.Add( {
+    name = "COD2019.Vector.Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,110},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/vector/weap_victor_fire_plr_01.ogg",
+		"weapons/cod2019/vector/weap_victor_fire_plr_02.ogg",
+		"weapons/cod2019/vector/weap_victor_fire_plr_03.ogg",
+		"weapons/cod2019/vector/weap_victor_fire_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
+    name = "COD2019.Vector.Silenced_Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,110},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/vector/weap_victor_fire_silenced_plr_01.ogg",
+		"weapons/cod2019/vector/weap_victor_fire_silenced_plr_02.ogg",
+		"weapons/cod2019/vector/weap_victor_fire_silenced_plr_03.ogg",
+		"weapons/cod2019/vector/weap_victor_fire_silenced_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
     name = "COD2019.Bizon.Fire",
     channel = 10,
     volume = 1.0,
--- lua/weapons/arc9_cod2019_aug.lua
+++ lua/weapons/arc9_cod2019_aug.lua
@@ -166,8 +166,8 @@ SWEP.TracerColor = Color(255, 255, 200) -- Color of tracers. Only works if trace
 -------------------------- POSITIONS
 
 SWEP.IronSights = {
-    Pos = Vector(-2.9, -3, -0.25),
-    Ang = Angle(0, 0, -1.5),
+    Pos = Vector(-3.45, -4, 0.7),
+    Ang = Angle(0, 0, -2.5),
     Magnification = 1.15,
     ViewModelFOV = 56,
 	CrosshairInSights = false
