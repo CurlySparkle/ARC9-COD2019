2e21d1f035fb6b3202112368cde8cef97984ddb1
May Lian <twilight_sparkle13@hotmail.com>
2023-02-22 00:27:35 -0300

    Added the UZI

--- lua/arc9/shared/sh_cod2019_sounds.lua
+++ lua/arc9/shared/sh_cod2019_sounds.lua
@@ -1,4 +1,49 @@
 sound.Add( {
+    name = "COD2019.Uzi.Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,105},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/uzi/weap_uzulu_fire_plr_01.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_fire_plr_02.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_fire_plr_03.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_fire_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
+    name = "COD2019.Uzi.Silenced_Fire",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,105},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/uzi/weap_uzulu_fire_silenced_plr_01.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_fire_silenced_plr_02.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_fire_silenced_plr_03.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_fire_silenced_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
+    name = "COD2019.Uzi.Trigger",
+    channel = 10,
+    volume = 1.0,
+    level = 120,
+    pitch = {90,105},
+	soundlevel = SNDLVL_GUNFIRE,
+    sound = {
+        "weapons/cod2019/uzi/weap_uzulu_prefire_plr_01.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_prefire_plr_02.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_prefire_plr_03.ogg",
+		"weapons/cod2019/uzi/weap_uzulu_prefire_plr_04.ogg",
+    }
+} )
+
+sound.Add( {
     name = "COD2019.Striker45.Fire",
     channel = 10,
     volume = 1.0,
