aff0e295b896a00f5977d4d77faae03673937194
May Lian <twilight_sparkle13@hotmail.com>
2023-04-16 02:24:40 -0400

    Added the RAAL

---
+++ lua/weapons/arc9_cod2019_raal.lua
@@ -0,0 +1,710 @@
+AddCSLuaFile()
+
+SWEP.Base = "arc9_cod2019_base"
+
+SWEP.Spawnable = true
+SWEP.Category = "ARC9 - MW2019"
+SWEP.SubCategory = "LMGs"
+
+SWEP.PrintName = "RAAL"
+
+SWEP.Class = "Light Machine Gun"
+SWEP.Trivia = {
+    ["Country of Origin"] = "Soviet Union",
+    ["Caliber"] = "7.62×54mmR",
+    ["Weight (Loaded)"] = "9.2 kg",
+    ["Projectile Weight"] = "181 gr",
+    ["Muzzle Velocity"] = "2,707 ft/s",
+    ["Muzzle Energy"] = "3,994 joules"
+}
+
+SWEP.Credits = {
+    Author = "Twilight Sparkle/Firmeteran",
+    Assets = "Infinity Ward/Valve/New World Interactive"
+}
+
+SWEP.Description = [[The Reconnaissance Auxiliary Assault Lightweight Machine Gun uses a low fire rate and a reciprocating barrel to mitigate the powerful .338 Norma Mag recoil. Advanced titanium construction keeps the weight manageable.]]
+
+SWEP.ViewModel = "models/weapons/cod2019/c_lmg_raal.mdl"
+SWEP.WorldModel = "models/weapons/w_snip_awp.mdl"
+
+SWEP.Slot = 2
+
+SWEP.MirrorVMWM = true
+SWEP.NoTPIKVMPos = true
+SWEP.WorldModelMirror = "models/weapons/cod2019/c_lmg_raal.mdl"
+SWEP.WorldModelOffset = {
+    Pos = Vector(-12, 6, -7.5),
+    Ang = Angle(-5, 0, 180),
+    TPIKPos = Vector(-10, 4, 0),
+    TPIKAng = Angle(0, 0, 175),
+    Scale = 1
+}
+
+-------------------------- DAMAGE PROFILE
+
+SWEP.DamageMax = 37 -- Damage done at point blank range
+SWEP.DamageMin = 34 -- Damage done at maximum range
+
+SWEP.DamageRand = 0.1 -- Damage varies randomly per shot by this fraction. 0.1 = +- 10% damage per shot.
+
+SWEP.RangeMin = 2000 -- How far bullets retain their maximum damage for.
+SWEP.RangeMax = 7000 -- In Hammer units, how far bullets can travel before dealing DamageMin.
+
+SWEP.Penetration = 15 -- Units of wood that can be penetrated by this gun.
+SWEP.RicochetChance = 0.15
+
+SWEP.ImpactForce = 12
+
+-------------------------- PHYS BULLET BALLISTICS
+
+SWEP.PhysBulletMuzzleVelocity = 3000 * 12
+SWEP.PhysBulletGravity = 1.5
+SWEP.PhysBulletDrag = 1.5
+
+-------------------------- MAGAZINE
+
+SWEP.Ammo = "ar2" -- What ammo type this gun uses.
+
+SWEP.ChamberSize = 0 -- The amount of rounds this gun can chamber.
+SWEP.ClipSize = 100 -- Self-explanatory.
+SWEP.SupplyLimit = 6 -- Amount of magazines of ammo this gun can take from an ARC9 supply crate.
+SWEP.SecondarySupplyLimit = 10 -- Amount of reserve UBGL magazines you can take.
+
+SWEP.ReloadInSights = false -- This weapon can aim down sights while reloading.
+SWEP.DrawCrosshair = true
+SWEP.Crosshair = true
+
+-------------------------- FIREMODES
+
+SWEP.RPM = 553
+
+SWEP.Firemodes = {
+    {
+        Mode = -1,
+    },
+}
+-------------------------- RECOIL
+
+-- General recoil multiplier
+SWEP.Recoil = 1.6
+
+SWEP.RecoilSeed = 24366
+
+SWEP.RecoilPatternDrift = 55
+
+-- These multipliers affect the predictible recoil by making the pattern taller, shorter, wider, or thinner.
+SWEP.RecoilUp = 0.8 -- Multiplier for vertical recoil
+SWEP.RecoilSide = 1 -- Multiplier for vertical recoil
+
+-- These values determine how much extra movement is applied to the recoil entirely randomly, like in a circle.
+-- This type of recoil CANNOT be predicted.
+SWEP.RecoilRandomUp = 0.3
+SWEP.RecoilRandomSide = 0.1
+
+SWEP.RecoilDissipationRate = 35 -- How much recoil dissipates per second.
+SWEP.RecoilResetTime = 0 -- How long the gun must go before the recoil pattern starts to reset.
+
+SWEP.RecoilAutoControl = 0.5 -- Multiplier for automatic recoil control.
+
+SWEP.RecoilKick = 2
+
+SWEP.RecoilMultCrouch = 0.8
+
+SWEP.RecoilMultMove = 1.25
+SWEP.RecoilAutoControlMultHipFire = 0.5
+SWEP.RecoilMultSights = 0.6
+
+-------------------------- VISUAL RECOIL
+
+SWEP.UseVisualRecoil = true
+SWEP.VisualRecoilMultSights = 0.5
+SWEP.VisualRecoilPunchSights = 15
+SWEP.VisualRecoilRollSights = 20
+SWEP.VisualRecoilPunch = 1
+SWEP.VisualRecoilUp = 0.9
+SWEP.VisualRecoilRoll = 1.4
+SWEP.VisualRecoilSide = 0
+
+-------------------------- SPREAD
+
+SWEP.Spread = 0.002
+
+SWEP.SpreadAddRecoil = 0.01
+SWEP.SpreadMultRecoil = 1.2
+SWEP.RecoilModifierCap = 2
+SWEP.RecoilModifierCapSights = 0
+
+
+SWEP.SpreadMultMove = 2
+--SWEP.SpreadAddMidAir = 0
+SWEP.SpreadAddHipFire = 0.05
+SWEP.SpreadAddCrouch = -0.03
+SWEP.SpreadAddSights = -0.5
+
+
+-------------------------- HANDLING
+
+SWEP.AimDownSightsTime = 0.8 -- How long it takes to go from hip fire to aiming down sights.
+SWEP.SprintToFireTime = 0.5 -- How long it takes to go from sprinting to being able to fire.
+
+-------------------------- MELEE
+
+SWEP.Bash = true
+SWEP.PrimaryBash = false
+SWEP.PreBashTime = 0.2
+SWEP.PostBashTime = 0.4
+
+-------------------------- TRACERS
+
+SWEP.TracerNum = 1 -- Tracer every X
+SWEP.TracerColor = Color(255, 255, 200) -- Color of tracers. Only works if tracer effect supports it. For physical bullets, this is compressed down to 9-bit color.
+
+-------------------------- POSITIONS
+
+SWEP.IronSights = {
+    Pos = Vector(-2.75, -1, 1),
+    Ang = Angle(0, 0, 2),
+    Magnification = 1.15,
+    ViewModelFOV = 56,
+	CrosshairInSights = false
+}
+
+SWEP.ViewModelFOVBase = 65
+
+SWEP.SprintPos = Vector(-1, 0, 0)
+SWEP.SprintAng = Angle(0, 0, 0)
+
+SWEP.SprintMidPoint = {
+    Pos = Vector(0, -1, -0.15),
+    Ang = Angle(0, 0, 0)
+}
+
+SWEP.ActivePos = Vector(0, 0, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.MovingMidPoint = {
+    Pos = Vector(0, -0.5, -0.5),
+    Ang = Angle(0, 0, 0)
+}
+
+SWEP.MovingPos = Vector(0, -0.5, -0.5)
+SWEP.MovingAng = Angle(0, 0, 0)
+
+SWEP.CrouchPos = Vector(-0.5, -0, -1)
+SWEP.CrouchAng = Angle(0, 0, -5)
+
+SWEP.CustomizeAng = Angle(90, 0, 0)
+SWEP.CustomizePos = Vector(19, 40, 5)
+SWEP.CustomizeSnapshotFOV = 90
+SWEP.CustomizeSnapshotPos = Vector(-1, 7, 5)
+SWEP.CustomizeSnapshotAng = Angle(0, 0, 0)
+SWEP.CustomizeNoRotate = false
+
+-------------------------- HoldTypes
+
+SWEP.HoldType = "rpg"
+SWEP.HoldTypeSprint = "rpg"
+SWEP.HoldTypeHolstered = "rpg"
+SWEP.HoldTypeSights = "rpg"
+SWEP.HoldTypeCustomize = "slam"
+SWEP.HoldTypeBlindfire = "pistol"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_MAGIC
+SWEP.AnimDraw = false
+
+-------------------------- EFFECTS
+
+SWEP.MuzzleParticle = "AC_muzzle_rifle"
+SWEP.AfterShotParticle = "AC_muzzle_smoke_barrel"
+SWEP.MuzzleEffectQCA = 1
+SWEP.ProceduralViewQCA = 1
+
+SWEP.CamQCA = 4
+SWEP.CamQCA_Mult = 1
+
+SWEP.ShellModel = "models/weapons/shared/lmg_bullet.mdl"
+SWEP.ShellCorrectAng = Angle(0, 0, 0)
+SWEP.ShellScale = 2.5
+SWEP.ShellPhysBox = Vector(1, 1, 1)
+
+SWEP.ShouldDropMag = false
+SWEP.ShouldDropMagEmpty = false
+SWEP.DropMagazineModel = "models/weapons/cod2019/mags/w_lmg_raal_mag.mdl" -- Set to a string or table to drop this magazine when reloading.
+SWEP.DropMagazineSounds = {"physics/metal/weapon_impact_soft1.wav", "physics/metal/weapon_impact_soft2.wav", "physics/metal/weapon_impact_soft3.wav"}
+SWEP.DropMagazineAmount = 1 -- Amount of mags to drop.
+SWEP.DropMagazineTime = 0.4
+SWEP.DropMagazineQCA = 3
+SWEP.DropMagazineAng = Angle(0, -90, 0)
+
+-------------------------- SOUNDS
+
+local path = "weapons/cod2019/raal/"
+
+SWEP.ShootSound = "COD2019.RAAL.Fire"
+SWEP.ShootSoundSilenced = "COD2019.RAAL.Silenced_Fire"
+SWEP.DistantShootSound = "CSGO.M249.Fire.Distance"
+SWEP.DryFireSound = "weapons/clipempty_rifle.wav"
+
+SWEP.FiremodeSound = "CSGO.Rifle.Switch_Mode"
+
+SWEP.EnterSightsSound = "COD2019.Iron.In_Rifle"
+SWEP.ExitSightsSound = "COD2019.Iron.Out_Rifle"
+
+SWEP.BulletBones = {
+    [1] = "j_bullet1",
+    [2] = "j_bullet2",
+    [3] = "j_bullet3",
+    [4] = "j_bullet4",
+	[5] = "j_bullet5",
+	[6] = "j_bullet6",
+	[7] = "j_bullet7",
+	[8] = "j_bullet8",
+	[9] = "j_bullet9",
+	[10] = "j_bullet10",
+	[11] = "j_bullet11",
+	[12] = "j_bullet12",
+	[13] = "j_bullet13",
+}
+
+SWEP.HideBones  = {
+    [1] = "j_mag2",
+}
+
+SWEP.TriggerDelay = 0.13 -- Set to > 0 to play the "trigger" animation before shooting. Delay time is based on this value.
+SWEP.TriggerDelay = true -- Add a delay before the weapon fires.
+SWEP.TriggerDelayTime = 0.13 -- Time until weapon fires.
+
+SWEP.TriggerDownSound = "weapons/cod2019/sa87/weap_lima86_fire_first_plr_01.ogg"
+SWEP.TriggerUpSound = ""
+
+SWEP.Animations = {
+    ["fire"] = {
+        Source = "shoot1",
+    },
+    ["fire_sights"] = {
+        Source = "shoot1_ads",
+    },
+    ["reload"] = {
+        Source = "reload_short",
+		MinProgress = 0.8,
+		MagSwapTime = 3,
+		DropMagAt = 2.9,
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 0
+            },
+            {
+                t = 0.1,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.7,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 1.15,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+        EventTable = {
+			{s = path .. "wfoly_lm_slima_reload_lower.ogg", t = 0/30},
+			{s = path .. "wfoly_lm_slima_reload_top_cover_open.ogg", t = 20/30},
+			{s = path .. "wfoly_lm_slima_reload_clean_tray.ogg", t = 35/30},
+			{s = path .. "wfoly_lm_slima_reload_arm_down.ogg", t = 66/30},
+			{s = path .. "wfoly_lm_slima_reload_magout.ogg", t = 84/30},
+			{s = path .. "wfoly_lm_slima_reload_lift.ogg", t = 102/30},
+			{s = path .. "wfoly_lm_slima_reload_maghit.ogg", t = 126/30},
+			{s = path .. "wfoly_lm_slima_reload_magin.ogg", t = 140/30},
+			{s = path .. "wfoly_lm_slima_reload_bullets_into_tray.ogg", t = 152/30},
+			{s = path .. "wfoly_lm_slima_reload_top_cover_close.ogg", t = 197/30},
+			{s = path .. "wfoly_lm_slima_reload_end.ogg", t = 214/30},
+        },
+    },
+    ["reload_empty"] = {
+        Source = "reload",
+		MinProgress = 0.9,
+		DropMagAt = 2.8,
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 0
+            },
+            {
+                t = 0.1,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.7,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 1.15,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+        EventTable = {
+			{s = path .. "wfoly_lm_slima_reload_empty_lower.ogg", t = 0/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_bolt_pull.ogg", t = 15/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_bolt_close.ogg", t = 29/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_settle.ogg", t = 41/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_top_cover_open.ogg", t = 58/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_clean_tray.ogg", t = 75/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_lift.ogg", t = 90/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_magout.ogg", t = 102/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_maghit.ogg", t = 138/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_magin.ogg", t = 148/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_bullets_into_tray.ogg", t = 165/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_top_cover_close.ogg", t = 204/30},
+			{s = path .. "wfoly_lm_slima_reload_empty_end.ogg", t = 226/30},
+        },
+    },
+    ["ready"] = {
+        Source = "draw",
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 0
+            },
+            {
+                t = 0.2,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.5,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.95,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+        EventTable = {
+            {s = path .. "wfoly_lm_slima_raise_first_lift.ogg", t = 0/30},
+            {s = path .. "wfoly_lm_slima_raise_first_bolt_pull.ogg", t = 14/30},
+			{s = path .. "wfoly_lm_slima_raise_first_bolt_close.ogg", t = 23/30},
+			{s = path .. "wfoly_lm_slima_raise_first_end.ogg", t = 35/30},
+        },
+    },
+    ["draw"] = {
+        Source = "draw_short",
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 1
+            },
+            {
+                t = 1,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+        EventTable = {
+            {s = path .. "wfoly_lm_slima_raise.ogg", t = 0/30},
+        },
+    },
+    ["holster"] = {
+        Source = "holster",
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 1
+            },
+            {
+                t = 0.5,
+                lhik = 0,
+                rhik = 1
+            },
+        },
+        EventTable = {
+            {s = path .. "wfoly_lm_slima_raise.ogg", t = 0/30},
+        },
+    },
+    ["idle"] = {
+        Source = "idle",
+    },
+    ["idle_sprint"] = {
+        Source = "sprint",
+    },
+    ["exit_sprint"] = {
+        Source = "sprint_out",
+		Mult = 2.3,
+    },
+    ["enter_sprint"] = {
+        Source = "sprint_in",
+		Mult = 2.3,
+    },
+    ["inspect"] = {
+        Source = "lookat01",
+		MinProgress = 0.1,
+		FireASAP = true,
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 0
+            },
+            {
+                t = 0.1,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.3,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.45,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+        EventTable = {
+            {s = path .. "wfoly_lm_slima_inspect_01.ogg", t = 0/30},
+			{s = path .. "wfoly_lm_slima_inspect_02.ogg", t = 25/30},
+			{s = path .. "wfoly_lm_slima_inspect_03.ogg", t = 47/30},
+			{s = path .. "wfoly_lm_slima_inspect_04.ogg", t = 79/30},
+			{s = path .. "wfoly_lm_slima_inspect_05.ogg", t = 121/30},
+			{s = path .. "wfoly_lm_slima_inspect_06.ogg", t = 168/30},
+        },
+    },
+    ["bash"] = {
+        Source = {"melee", "melee2"},
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 0
+            },
+            {
+                t = 0.2,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.5,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.8,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+    },
+}
+
+-------------------------- ATTACHMENTS
+
+SWEP.Hook_Think	= ARC9.COD2019.BlendEmpty2
+
+SWEP.DefaultBodygroups = "00000000000000"
+
+SWEP.AttachmentTableOverrides = {
+    ["arc9_stat_proscreen_main"] = {
+    ModelOffset = Vector(16, 0, -1.2),
+	ModelAngleOffset = Angle(0, 0, 0),
+	Scale = 0.9,
+    },
+    ["go_grip_angled"] = {
+    ModelOffset = Vector(0, 0, 0.1),
+    },
+}
+
+SWEP.AttachmentElements = {
+    ["body_none"] = {
+        Bodygroups = {
+            {0,1},
+        },
+    },
+    ["mag_none"] = {
+        Bodygroups = {
+            {1,1},
+        },
+    },
+    ["barrel_none"] = {
+        Bodygroups = {
+            {2,1},
+        },
+    },
+    ["muzzle_none"] = {
+        Bodygroups = {
+            {3,1},
+        },
+    },
+    ["stock_adapter"] = {
+        Bodygroups = {
+            {4,1},
+        },
+    },    
+	["stock_none"] = {
+        Bodygroups = {
+            {4,2},
+        },
+    },
+	["sight_none"] = {
+        Bodygroups = {
+            {5,1},
+        },
+    },
+	["bipod_none"] = {
+        Bodygroups = {
+            {7,1},
+        },
+    },
+	["grip_rail"] = {
+        Bodygroups = {
+            {9,1},
+        },
+    },
+}
+
+-- SWEP.Hook_ModifyBodygroups = function(wep, data)
+    -- local model = data.model
+    -- if wep:HasElement("stock_retract") then model:SetBodygroup(4,1) end
+-- end
+
+SWEP.Attachments = {
+    {
+        PrintName = "Barrels",
+        DefaultAttName = "Standard Barrel",
+        Category = "cod2019_raal_barrel",
+        Bone = "tag_barrel_attach",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+    },
+    {
+        PrintName = "Muzzle",
+        DefaultAttName = "Standard Muzzle",
+        Category = "muzzle",
+        Bone = "tag_silencer",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+		InstalledElements = {"muzzle_none"},
+		Scale = 1,
+    },
+    {
+        PrintName = "Optics",
+        Bone = "tag_holo",
+        Pos = Vector(1.5, 0, -0.05),
+        Ang = Angle(0, 0, 0),
+        Category = {"csgo_optic",},
+        CorrectiveAng = Angle(0, 0, 0),
+		InstalledElements = {"sight_none"},
+    },
+    {
+        PrintName = "Tactical",
+        DefaultAttName = "Default",
+        Category = "csgo_tac",
+        Bone = "tag_laser_attach",
+        Pos = Vector(-4, -0.35, 0.45),
+        Ang = Angle(0, 0, -90),
+		--InstalledElements = {"rail_laser"},
+    },
+    {
+        PrintName = "Grips",
+        DefaultAttName = "Default",
+        Category = "grip",
+        Bone = "tag_grip_attach",
+        Pos = Vector(1.2, 1.3, -3),
+        Ang = Angle(0, 0, 180),
+		Scale = 1,
+		InstalledElements = {"bipod_none","grip_rail"},
+    },
+    {
+        PrintName = "Stock",
+        DefaultAttName = "Standard Stock",
+        Category = {"cod2019_raal_stock","cod2019_tube"},
+        Bone = "tag_stock_attach",
+        Pos = Vector(0, 0, 0.7),
+        Ang = Angle(0, 0, 0),
+		InstalledElements = {"stock_adapter"},
+    },
+    {
+        PrintName = "Ammo",
+        Bone = "j_mag1",
+        Category = {"go_ammo"},
+        Pos = Vector(0, 0, -1.5),
+        Ang = Angle(0, 0, 0),
+    },
+    {
+        PrintName = "Mag",
+		Bone = "j_mag1",
+        Category = "cod2019_raal_mag",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+    },
+    {
+        PrintName = "Perk",
+        Category = "go_perk"
+    },
+    {
+        PrintName = "Skins",
+        --Bone = "v_weapon.Clip",
+        Category = "cod2019_skins_m91",
+		CosmeticOnly = true,
+    },
+    {
+        PrintName = "Cosmetic",
+        Category = {"universal_camo"},
+        CosmeticOnly = true,
+    },
+    {
+        PrintName = "Stickers",
+        StickerModel = "models/weapons/cod2019/stickers/lmg_raal_decal_a.mdl",
+        Category = "stickers",
+    },
+    {
+        PrintName = "Stickers",
+        StickerModel = "models/weapons/cod2019/stickers/lmg_raal_decal_b.mdl",
+        Category = "stickers",
+    },
+    {
+        PrintName = "Stickers",
+        StickerModel = "models/weapons/cod2019/stickers/lmg_raal_decal_c.mdl",
+        Category = "stickers",
+    },
+    {
+        PrintName = "Stickers",
+        StickerModel = "models/weapons/cod2019/stickers/lmg_raal_decal_d.mdl",
+        Category = "stickers",
+    },
+    {
+        PrintName = "Charm",
+        Category = "charm",
+        Bone = "tag_cosmetic",
+        Pos = Vector(0.5, 0, 0),
+        Ang = Angle(0, 0, 0),
+		Scale = 1.5,
+    },
+    {
+        PrintName = "Stats",
+        Category = {"killcounter","killcounter2"},
+        Bone = "tag_cosmetic",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+		CosmeticOnly = true,
+    },
+}
+
+SWEP.GripPoseParam = 5
+SWEP.GripPoseParam2 = 0.5
+SWEP.CodAngledGripPoseParam = 4
