8152dcbc28a2c0b58e7d295a553e76163008de89
May Lian <twilight_sparkle13@hotmail.com>
2023-08-10 15:11:12 -0400

    moved eft/gsr support to here

---
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_support_eft.lua
@@ -0,0 +1,107 @@
+local ATT = {}
+-----------------------------------------      cod2019_eft_grips
+ATT = {}
+
+ATT.PrintName = "Escape from Tarkov Grips"
+ATT.CompactName = "EFTG"
+ATT.Description = "Enables the ability to equip grips from the EFT packs.\n\nNote: Do remember that there are glitches/bugs/clipping."
+
+ATT.SortOrder = 10000
+ATT.Category = "cod2019_grip"
+ATT.MenuCategory = "ARC9 - MW19 Attachments"
+ATT.Attachments = {
+    {
+        PrintName = "Grip",
+        Category = {"eft_foregrip_small", "eft_foregrip_large"},
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 180),
+        Icon_Offset = Vector(1.5, 0, -1.75),
+		RejectAttachments = {["eft_foregrip_sturm"] = true}
+    },
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_eft_grips")
+-----------------------------------------      cod2019_eft_muzzle
+ATT = {}
+
+ATT.PrintName = "Escape from Tarkov Muzzles"
+ATT.CompactName = "EFTM"
+ATT.Description = "Enables the ability to equip muzzles from the EFT packs.\n\nNote: Do remember that there are glitches/bugs/clipping."
+
+ATT.SortOrder = 10000
+ATT.Category = "cod2019_muzzle"
+ATT.MenuCategory = "ARC9 - MW19 Attachments"
+ATT.Attachments = {
+    {
+        PrintName = "Muzzle",
+        Category = {"eft_ar15_muzzle"},
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, -1.75),
+    },
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_eft_muzzle")
+-----------------------------------------      cod2019_eft_scopes
+ATT = {}
+
+ATT.PrintName = "Escape from Tarkov Scopes"
+ATT.CompactName = "EFTS"
+ATT.Description = "Enables the ability to equip scopes from the EFT packs.\n\nNote: Do remember that there are glitches/bugs/clipping."
+
+ATT.SortOrder = 10000
+ATT.Category = "cod2019_optic"
+ATT.MenuCategory = "ARC9 - MW19 Attachments"
+ATT.Attachments = {
+    {
+        PrintName = "Optics",
+        Category = {"eft_optic_large", "eft_optic_medium", "eft_optic_small"},
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(1.5, 0, 0),
+    },
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_eft_scopes")
+-----------------------------------------      cod2019_eft_scopes_pistol
+ATT = {}
+
+ATT.PrintName = "Escape from Tarkov Scopes"
+ATT.CompactName = "EFTS"
+ATT.Description = "Enables the ability to equip scopes from the EFT packs.\n\nNote: Do remember that there are glitches/bugs/clipping."
+
+ATT.SortOrder = 10000
+ATT.Category = "cod2019_optics_pistols_alt"
+ATT.MenuCategory = "ARC9 - MW19 Attachments"
+ATT.Attachments = {
+    {
+        PrintName = "Optics",
+        Category = {"eft_optic_small"},
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(1.5, 0, 0),
+    },
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_eft_scopes_pistol")
+-----------------------------------------      cod2019_eft_stocks
+ATT = {}
+
+ATT.PrintName = "Escape from Tarkov Stocks"
+ATT.CompactName = "EFTS"
+ATT.Description = "Enables the ability to equip stocks from the EFT packs.\n\nNote: Do remember that there are glitches/bugs/clipping."
+
+ATT.SortOrder = 10000
+ATT.Category = "cod2019_stocks"
+ATT.MenuCategory = "ARC9 - MW19 Attachments"
+ATT.Attachments = {
+    {
+        PrintName = "Stock",
+        Category = {"eft_ar_stock"},
+        Pos = Vector(5, 0, 0.85),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(1.5, 0, 0.85),
+    },
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_eft_stocks")
---
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_support_gsr.lua
@@ -0,0 +1,171 @@
+local ATT = {}
+
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = [[GSR Tacticals]]
+ATT.CompactName = [[GSR Tac]]
+ATT.Description = [[Lets you use tactical attachments from GSR.]]
+--ATT.Icon = Material("entities/attachs/cod2019_origin12_suppressor.png", "mips smooth")
+
+ATT.SortOrder = 1
+ATT.AutoStats = true
+ATT.Category = "cod2019_tac_pistols"
+--ATT.Folder = "Suppresors"
+
+--ATT.Model = "models/weapons/cod2019/attachs/silencers/attachment_vm_sh_oscar12_silencer03.mdl"
+
+ATT.Attachments = {
+    {
+        PrintName = "Tactical",
+        Pos = Vector(-0.5, 0, -0.2),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, 0),
+        Category = "csgo_tac_pistols",
+		Scale = 1
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_attach_custom_laser_pstl")
+------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = [[GSR Optics]]
+ATT.CompactName = [[GSR Optics]]
+ATT.Description = [[Lets you use optic attachments from GSR.]]
+--ATT.Icon = Material("entities/attachs/cod2019_origin12_suppressor.png", "mips smooth")
+
+ATT.SortOrder = 1
+ATT.AutoStats = true
+ATT.Category = "cod2019_optic"
+--ATT.Folder = "Suppresors"
+
+--ATT.Model = "models/weapons/cod2019/attachs/silencers/attachment_vm_sh_oscar12_silencer03.mdl"
+
+ATT.Attachments = {
+    {
+        PrintName = "Optics",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, 0),
+        Category = "csgo_optic",
+		Scale = 1
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_attach_custom_optics")
+------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = [[GSR Optics]]
+ATT.CompactName = [[GSR Optics]]
+ATT.Description = [[Lets you use pistol optics from GSR.]]
+--ATT.Icon = Material("entities/attachs/cod2019_origin12_suppressor.png", "mips smooth")
+
+ATT.SortOrder = 1
+ATT.AutoStats = true
+ATT.Category = "cod2019_optics_pistols_alt"
+--ATT.Folder = "Suppresors"
+
+--ATT.Model = "models/weapons/cod2019/attachs/silencers/attachment_vm_sh_oscar12_silencer03.mdl"
+
+ATT.Attachments = {
+    {
+        PrintName = "Optics",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, 0),
+        Category = "csgo_optics_pistols",
+		Scale = 1
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_attach_custom_optics_pistols")
+------------------------------------------------------------------ MUZZLES
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = [[GSR Muzzles]]
+ATT.CompactName = [[GSR Muzz]]
+ATT.Description = [[Lets you use muzzles from GSR.]]
+--ATT.Icon = Material("entities/attachs/cod2019_origin12_suppressor.png", "mips smooth")
+
+ATT.SortOrder = 1
+ATT.AutoStats = true
+ATT.Category = "cod2019_muzzle_shot"
+--ATT.Folder = "Suppresors"
+
+--ATT.Model = "models/weapons/cod2019/attachs/silencers/attachment_vm_sh_oscar12_silencer03.mdl"
+
+ATT.Attachments = {
+    {
+        PrintName = "Muzzle",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, 0),
+        Category = {"muzzle_shotgun","muzzle"},
+		Scale = 1
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_attach_custom_muzzles")
+------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = [[GSR Muzzles]]
+ATT.CompactName = [[GSR Muzz]]
+ATT.Description = [[Lets you use muzzles from GSR.]]
+--ATT.Icon = Material("entities/attachs/cod2019_origin12_suppressor.png", "mips smooth")
+
+ATT.SortOrder = 1
+ATT.AutoStats = true
+ATT.Category = "cod2019_muzzle_pistols"
+--ATT.Folder = "Suppresors"
+
+--ATT.Model = "models/weapons/cod2019/attachs/silencers/attachment_vm_sh_oscar12_silencer03.mdl"
+
+ATT.Attachments = {
+    {
+        PrintName = "Muzzle",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, 0),
+        Category = {"muzzle_pistols","muzzle"},
+		Scale = 1
+    }
+}
+
+ATT.Scale = 0.8
+
+ARC9.LoadAttachment(ATT, "cod2019_attach_custom_muzzles_pistols")
+------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = [[GSR Muzzles]]
+ATT.CompactName = [[GSR Muzz]]
+ATT.Description = [[Lets you use muzzles from GSR.]]
+--ATT.Icon = Material("entities/attachs/cod2019_origin12_suppressor.png", "mips smooth")
+
+ATT.SortOrder = 1
+ATT.AutoStats = true
+ATT.Category = "cod2019_muzzle"
+--ATT.Folder = "Suppresors"
+
+--ATT.Model = "models/weapons/cod2019/attachs/silencers/attachment_vm_sh_oscar12_silencer03.mdl"
+
+ATT.Attachments = {
+    {
+        PrintName = "Muzzle",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, 0),
+        Category = {"muzzle"},
+		Scale = 1
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_attach_custom_muzzles_alt")
