3bb67a6d4da8fb3b2435de7d0da4670350031963
MokaAkashiya85 <ThaERROR404@gmail.com>
2023-12-31 23:40:38 +0100

    Update mw19_en.lua
    - Updated localizations and added many new ones to non-attachment pack-specific attachments, such as ammunition types.

--- lua/arc9/common/localization/mw19_en.lua
+++ lua/arc9/common/localization/mw19_en.lua
@@ -130,8 +130,115 @@ L["mw19_category_pumpgrip"] = "Pump Grip"
 -- Specific to the SP-R 208 Marksman Rifle
 L["mw19_category_boltassembly"] = "Bolt Assembly"
 
+//////////////////// Folder Names
+-- Other
+L["mw19_folder_other2"] = "Other" -- UO
+
 //////////////////// Firemodes
 -- L["mw19_firemode_pump"] = "PUMP"
+
+//////////////////////////////////////////////////////////////////////
+///////////////////////////// Base Attachments
+////////////////////  Ammo
+L["cod2019_ammo_ap.printname"] = "Armor Piercing Rounds" -- UO
+L["cod2019_ammo_ap.compactname"] = "AP" -- UO
+L["cod2019_ammo_ap.description"] = "Ammunition made of hardened material designed to pierce armor.\nHigh penetrating capabilities, but wounding potential is reduced." -- UO
+
+L["cod2019_ammo_hr.printname"] = "High Ricochet Rounds" -- UO
+L["cod2019_ammo_hr.compactname"] = "HR" -- UO
+L["cod2019_ammo_hr.description"] = "Ammunition made of soft material designed to ricochet off of surfaces.\nHigh ricochet capabilities, but less damage output." -- UO
+
+L["cod2019_ammo_fra.printname"] = "Hollow Point Rounds" -- UO
+L["cod2019_ammo_fra.compactname"] = "HP" -- UO
+L["cod2019_ammo_fra.description"] = "Ammunition with a hollow tip that expands upon hitting its targets, incapacitating them more easily.\nWidely used in police and civilian markets due to its inability to penetrate and ricochet." -- UO
+
+L["cod2019_ammo_subsonic.printname"] = "Low Profile Rounds" -- UO
+L["cod2019_ammo_subsonic.compactname"] = "LP" -- UO
+L["cod2019_ammo_subsonic.description"] = "Ammunition with reduced powder loads made for easier weapon handling.\nImproves weapon recoil control at the cost of weapon range." -- UO
+
+L["cod2019_ammo_tracer_col.printname"] = "Weapon Color Tracer Rounds" -- UO
+L["cod2019_ammo_tracer_col.compactname"] = "WCTR" -- UO
+L["cod2019_ammo_tracer_col.description"] = "Ammunition with tracer points with the color matching the user's physics gun color.\n\nPretty dope, right?" -- UO
+
+L["cod2019_ammo_db.printname"] = "Incendiary Rounds" -- UO
+L["cod2019_ammo_db.compactname"] = "Inc." -- UO
+L["cod2019_ammo_db.description"] = "Incendiary ammunition that light targets on fire on hit." -- UO
+-- L["cod2019_ammo_db.description"] = "Incendiary ammunition that has a change to light targets on fire on hit." -- UO
+
+L["cod2019_ammo_he.printname"] = "High Explosive Rounds" -- UO
+L["cod2019_ammo_he.compactname"] = "HE" -- UO
+L["cod2019_ammo_he.description"] = "Explosive ammunition that sacrifice direct damage in favor of dealing damage in a radius around the impact area.\nThe effect is spectacular, but very little explosive filler can be put inside the bullets." -- UO
+
+-- Shotgun Ammo
+L["cod2019_ammo_sg_magnum.printname"] = "8.5mm Magnum Buckshot" -- UO
+L["cod2019_ammo_sg_magnum.compactname"] = "MAG" -- UO
+L["cod2019_ammo_sg_magnum.description"] = "High-powered buckshot loaded with fewer but larger lead pellets.\nProvides better close range power, but worsened range and accuracy." -- UO
+
+-- L["cod2019_ammo_sg_bird.printname"] = "3.5mm Birdshot" -- UO
+-- L["cod2019_ammo_sg_bird.compactname"] = "BIRD" -- UO
+-- L["cod2019_ammo_sg_bird.description"] = "Reduced diameter load containing a higher quantity of pallets.\nThe spread is increased which makes hitting more targets easier." -- UO
+
+L["cod2019_ammo_sg_slug.printname"] = "70mm Full-bore Slug" -- UO
+L["cod2019_ammo_sg_slug.compactname"] = "SLUG" -- UO
+L["cod2019_ammo_sg_slug.description"] = "A single large projectile loaded into a shotgun shell.\nThe aerodynamic pellet has much higher effective range." -- UO
+
+-- L["cod2019_ammo_sg_triple.printname"] = "70mm Triple Hit Slugs" -- UO
+-- L["cod2019_ammo_sg_triple.compactname"] = "TRI" -- UO
+-- L["cod2019_ammo_sg_triple.description"] = "Load containing three short, slug-sized pellets.\nThe slugs provide increased effective range and accuracy, but not as much as the full-bore slug." -- UO
+
+L["cod2019_ammo_sg_flechette.printname"] = "Flechette Shells" -- UO
+L["cod2019_ammo_sg_flechette.compactname"] = "FLE" -- UO
+L["cod2019_ammo_sg_flechette.description"] = "Load containing thin, dart-shaped projectiles.\nProvides tighter spread and increased range alongside increased penetration, but at the cost of reduced stopping power." -- UO
+
+L["cod2019_ammo_sg_frag.printname"] = "70mm \"FRAG-12\" High Explosive" -- UO
+L["cod2019_ammo_sg_frag.compactname"] = "FRAG-12" -- UO
+L["cod2019_ammo_sg_frag.description"] = "Load containing a single projectile filled with an explosive compound.\nDeals damage around a small radius at the impact area." -- UO
+
+L["cod2019_ammo_sg_rebounder.printname"] = "10mm Rebounder Rounds" -- UO
+L["cod2019_ammo_sg_rebounder.compactname"] = "RBD" -- UO
+L["cod2019_ammo_sg_rebounder.description"] = "Load containing specialized pellets that have a high chance to ricochet.\nThe pellets will bounce regardless of angle, dealing increased damage the more it ricochets.\nIt does, however, have reduced damage output on direct impact." -- UO
+
+L["cod2019_ammo_sg_frag_x4.printname"] = "70mm \"FRAG-12\" High Explosive x4" -- UO
+L["cod2019_ammo_sg_frag_x4.compactname"] = "FRAG-12 x4" -- UO
+L["cod2019_ammo_sg_frag_x4.description"] = "Load containing four projectiles filled with explosive compounds.\nDeals damage around a small radius at the impact area." -- UO
+
+L["cod2019_ammo_sg_db.printname"] = "70mm \"Dragon's Breach\"" -- UO
+L["cod2019_ammo_sg_db.compactname"] = "DB" -- UO
+L["cod2019_ammo_sg_db.description"] = "Load containing magnesium pellets that lights a target on fire on hit." -- UO
+
+////////////////////  Etc.
+L["cod2019_etc_ammo_extend.printname"] = "Lightweight Extended Mag" -- UO
+L["cod2019_etc_ammo_extend.compactname"] = "Light EM" -- UO
+L["cod2019_etc_ammo_extend.description"] = "Custom extended magazines holding <color=100,255,100>20%</color> more ammunition." -- UO
+
+L["cod2019_attach_grip_support.printname"] = "Grip Support" -- UO
+L["cod2019_attach_grip_support.compactname"] = "Grip S." -- UO
+L["cod2019_attach_grip_support.description"] = "Allows the installation of foregrips onto handguns. <color=255,100,100>Cannot apply lasers as well.</color>" -- UO
+
+L["cod2019_view_alt_pistol.printname"] = "Alternative View" -- UO
+L["cod2019_view_alt_pistol.compactname"] = "Alt. View" -- UO
+L["cod2019_view_alt_pistol.description"] = "Alternative viewing angle." -- UO
+
+L["cod2019_attach_laser_rail.printname"] = "Laser Attachment" -- UO
+L["cod2019_attach_laser_rail.compactname"] = "Laser Att." -- UO
+L["cod2019_attach_laser_rail.description"] = "Allows the installation of laser sights onto your weapon." -- UO
+
+L["cod2019_attach_laser_rail_2.printname"] = "Laser Attachment" -- UO
+L["cod2019_attach_laser_rail_2.compactname"] = "Laser Att." -- UO
+L["cod2019_attach_laser_rail_2.description"] = "Allows the installation of small, cylindrical laser sights onto your weapon." -- UO
+
+L["cod2019_attach_laser_cylinder.printname"] = "Laser Attachment" -- UO
+L["cod2019_attach_laser_cylinder.compactname"] = "Laser Att." -- UO
+L["cod2019_attach_laser_cylinder.description"] = "Allows the installation of laser sights onto your weapon." -- UO
+
+L["cod2019_attach_laser_cylinder_2.printname"] = "Laser Attachment" -- UO
+L["cod2019_attach_laser_cylinder_2.compactname"] = "Laser Att." -- UO
+L["cod2019_attach_laser_cylinder_2.description"] = "Allows the installation of small, cylindrical laser sights onto your weapon." -- UO
+
+////////////////////  Grips
+-- L["cod2019_attach_grip_foregrip_side.printname"] = "Lightweight Extended Mag" -- UO
+-- L["cod2019_attach_grip_foregrip_side.compactname"] = "Light EM" -- UO
+-- L["cod2019_attach_grip_foregrip_side.description"] = "Custom extended magazines holding <color=100,255,100>20%</color> more ammunition." -- UO
 
 //////////////////////////////////////////////////////////////////////
 ///////////////////////////// Weapon Names, Descriptions and unique attachments
