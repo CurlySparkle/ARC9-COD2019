24679e36a6c06162d9268d029ef2068bb52cbda3
May Lian <twilight_sparkle13@hotmail.com>
2024-02-18 15:53:03 -0300

    pi

--- lua/weapons/arc9_cod2019_pi_50gs.lua
+++ lua/weapons/arc9_cod2019_pi_50gs.lua
@@ -65,10 +65,10 @@ SWEP.BodyDamageMults = {
     [HITGROUP_HEAD] = 1.615,
     [HITGROUP_CHEST] = 1,
     [HITGROUP_STOMACH] = 1,
-    [HITGROUP_LEFTARM] = 1,
-    [HITGROUP_RIGHTARM] = 1,
-    [HITGROUP_LEFTLEG] = 1,
-    [HITGROUP_RIGHTLEG] = 1,
+    [HITGROUP_LEFTARM] = 0.9,
+    [HITGROUP_RIGHTARM] = 0.9,
+    [HITGROUP_LEFTLEG] = 0.9,
+    [HITGROUP_RIGHTLEG] = 0.9,
 }
 
 -------------------------- PHYS BULLET BALLISTICS
@@ -965,4 +965,14 @@ SWEP.RangeMin = 16 / ARC9.HUToM
 SWEP.RangeMax = 17 / ARC9.HUToM
 
 SWEP.RPM = 167
+
+SWEP.BodyDamageMults = {
+    [HITGROUP_HEAD] = 1.615,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_STOMACH] = 1,
+    [HITGROUP_LEFTARM] = 1,
+    [HITGROUP_RIGHTARM] = 1,
+    [HITGROUP_LEFTLEG] = 1,
+    [HITGROUP_RIGHTLEG] = 1,
+}
 end
