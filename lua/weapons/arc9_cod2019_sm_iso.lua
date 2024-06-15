061419580ca79954e94ee9bd2a94f1661eb6d81c
MokaAkashiya85 <ThaERROR404@gmail.com>
2024-06-15 14:42:55 +0200

    Spread
    - All SMG's (except Fennec): Reduced spread while aiming.

--- lua/weapons/arc9_cod2019_sm_iso.lua
+++ lua/weapons/arc9_cod2019_sm_iso.lua
@@ -172,7 +172,7 @@ SWEP.SpreadAddHipFire = SWEP.Spread * 0
 SWEP.SpreadAddMove = SWEP.Spread * 1
 SWEP.SpreadAddMidAir = SWEP.Spread * 0.5
 SWEP.SpreadAddCrouch = -SWEP.Spread * 0.1
-SWEP.SpreadAddSights = -SWEP.Spread * 1.25
+SWEP.SpreadAddSights = -SWEP.Spread * 1.75
 
 SWEP.SpreadMultRecoil = 1
 SWEP.RecoilModifierCap = SWEP.RecoilMax
