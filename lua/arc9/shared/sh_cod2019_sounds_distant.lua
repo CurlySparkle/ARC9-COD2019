bad165b47e4b9d82f387941032bf994684a320b3
May Lian <twilight_sparkle13@hotmail.com>
2025-05-15 11:35:50 -0400

    some

---
+++ lua/arc9/shared/sh_cod2019_sounds_distant.lua
@@ -0,0 +1,471 @@
+AddCSLuaFile()
+----------- OUTSIDE ------------
+sound.Add({
+    name = "Distant_AR.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/ar/weap_ar_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR_Sup.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    volume = 1,
+    sound = "weapons/cod2019/shared/atmo/ar/weap_ar_sup_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR2.Outside", 
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/ar/weap_ar2_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR2_Sup.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "weapons/cod2019/shared/atmo/ar/weap_ar2_sup_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR3.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/ar/weap_ar3_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR4.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/ar/weap_ar4_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR5.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/ar/weap_ar5_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR6.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/ar/weap_ar6_fire_plr_atmo_ext1_01.ogg"
+})
+
+--- BR ---
+sound.Add({
+    name = "Distant_BR1.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/br/weap_br1_fire_plr_atmo_ext1_01.wav",
+})
+
+sound.Add({
+    name = "Distant_BR2.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/br/weap_br2_fire_plr_atmo_ext1_01.wav",
+})
+
+--- DMR ---
+sound.Add({
+    name = "Distant_DMR.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "weapons/cod2019/shared/atmo/dmr/weap_dmr_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_DMR_Sup.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    --level = 140,
+    volume = 1,
+    sound = "weapons/cod2019/shared/atmo/dmr/weap_dmr_sup_fire_plr_atmo_ext1_01.ogg"
+})
+
+--- LMG ---
+sound.Add({
+    name = "Distant_LMG.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "weapons/cod2019/shared/atmo/lmg/weap_lmg_fire_plr_atmo_ext1_01.wav"
+})
+
+--- M203 ---
+sound.Add({
+    name = "Distant_M203.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/launcher/weap_mike203_fire_plr_atmos_01.ogg"
+})
+
+--- PISTOLS ---
+sound.Add({
+    name = "Distant_Pistol.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    pitch = {80,110},
+    volume =     1,
+    sound = "^weapons/cod2019/shared/atmo/pistol/weap_pistol_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_Pistol_Sup.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/pistol/weap_pistol_sup_fire_plr_atmo_ext1_01.ogg"
+})
+
+sound.Add({
+    name = "Distant_Pistol_Mag.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_Pistol_Mag_Sup.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    pitch = {80,110},
+    sound = {"weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_sup_fire_plr_atmo_ext1_01.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_sup_fire_plr_atmo_ext1_02.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_sup_fire_plr_atmo_ext1_03.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_sup_fire_plr_atmo_ext1_04.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_sup_fire_plr_atmo_ext1_05.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_sup_fire_plr_atmo_ext1_06.ogg"}
+})
+
+sound.Add({
+    name = "Distant_Pistol_Mag2.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/pistol/weap_pistol_mag_fire_plr_atmo_ext2_01.wav"
+})
+
+
+--- SHOTGUNS ---
+
+sound.Add({
+    name = "Distant_Shotgun.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/shotgun/weap_shotgun_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_Shotgun2.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/shotgun/weap_shotgunsup_fire_plr_atmo_ext1_01.wav"
+})
+
+
+--- SMG's ---
+
+sound.Add({
+    name = "Distant_SMG.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/smg/weap_smg_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_SMG_Sup.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = {"weapons/cod2019/shared/atmo/smg/weap_smg_sup_fire_plr_atmo_ext1_01.ogg",
+            "weapons/cod2019/shared/atmo/smg/weap_smg_sup_fire_plr_atmo_ext1_02.ogg",
+            "weapons/cod2019/shared/atmo/smg/weap_smg_sup_fire_plr_atmo_ext1_03.ogg",
+            "weapons/cod2019/shared/atmo/smg/weap_smg_sup_fire_plr_atmo_ext1_04.ogg",
+            "weapons/cod2019/shared/atmo/smg/weap_smg_sup_fire_plr_atmo_ext1_05.ogg",
+            "weapons/cod2019/shared/atmo/smg/weap_smg_sup_fire_plr_atmo_ext1_06.ogg"}
+})
+
+sound.Add({
+    name = "Distant_SMG2.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/smg/weap_smg2_fire_plr_atmo_ext1_01.wav"
+})
+
+--- Snipers ---
+
+sound.Add({
+    name = "Distant_Sniper.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "weapons/cod2019/shared/atmo/sniper/weap_sniper_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_Sniper2.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "weapons/cod2019/shared/atmo/sniper/weap_sniper2_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_Sniper3.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/sniper/weap_sniper3_fire_plr_atmo_ext1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_Sniper_Sup.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "weapons/cod2019/shared/atmo/sniper/weap_sniper_sup_fire_plr_atmo_ext1_01.wav"
+})
+
+---  Western ---
+sound.Add({
+    name = "Distant_Western.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/western/weap_western_fire_plr_atmo_ext1_01.wav"
+})
+
+----------- ATMO INSIDE ------------
+
+--- AR ---
+sound.Add({
+    name = "Distant_AR.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "weapons/cod2019/shared/atmo/ar/weap_ar_fire_plr_atmo_int1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_AR_Sup.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    pitch = {80,110},
+    volume = 1,
+    sound = {"weapons/cod2019/shared/atmo/ar/weap_ar_sup_fire_plr_atmo_int1_01.ogg",
+            "weapons/cod2019/shared/atmo/ar/weap_ar_sup_fire_plr_atmo_int1_02.ogg",
+            "weapons/cod2019/shared/atmo/ar/weap_ar_sup_fire_plr_atmo_int1_03.ogg",
+            "weapons/cod2019/shared/atmo/ar/weap_ar_sup_fire_plr_atmo_int1_04.ogg",
+            "weapons/cod2019/shared/atmo/ar/weap_ar_sup_fire_plr_atmo_int1_05.ogg",
+            "weapons/cod2019/shared/atmo/ar/weap_ar_sup_fire_plr_atmo_int1_06.ogg"}
+})
+
+--- DMR ---
+sound.Add({
+    name = "Distant_DMR_Sup.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    pitch = {80,110},
+    volume = 1,
+    sound = {"weapons/cod2019/shared/atmo/dmr/weap_dmr_sup_fire_plr_atmo_int1_01.ogg",
+            "weapons/cod2019/shared/atmo/dmr/weap_dmr_sup_fire_plr_atmo_int1_02.ogg",
+            "weapons/cod2019/shared/atmo/dmr/weap_dmr_sup_fire_plr_atmo_int1_03.ogg",
+            "weapons/cod2019/shared/atmo/dmr/weap_dmr_sup_fire_plr_atmo_int1_04.ogg",
+            "weapons/cod2019/shared/atmo/dmr/weap_dmr_sup_fire_plr_atmo_int1_05.ogg",
+            "weapons/cod2019/shared/atmo/dmr/weap_dmr_sup_fire_plr_atmo_int1_06.ogg"}
+})
+
+--- LMG ---
+sound.Add({
+    name = "Distant_LMG.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/lmg/weap_lmg_fire_plr_atmo_int1_01.wav"
+})
+
+--- M203 ---
+sound.Add({
+    name = "Distant_Mike203.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "weapons/cod2019/shared/atmo/launcher/weap_mike203_fire_plr_atmos_int1_01.ogg"
+})
+
+--- Pistol ---
+sound.Add({
+    name = "Distant_Pistol.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/pistol/weap_pistol_fire_plr_atmo_int1_01.wav"
+})
+
+sound.Add({
+    name = "Distant_Pistol_Sup.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    pitch = {80,110},
+    volume = 1,
+    sound = {"weapons/cod2019/shared/atmo/pistol/weap_pistol_sup_fire_plr_atmo_int1_01.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_sup_fire_plr_atmo_int1_02.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_sup_fire_plr_atmo_int1_03.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_sup_fire_plr_atmo_int1_04.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_sup_fire_plr_atmo_int1_05.ogg",
+            "weapons/cod2019/shared/atmo/pistol/weap_pistol_sup_fire_plr_atmo_int1_06.ogg"}
+})
+
+--- Shotguns ---
+sound.Add({
+    name = "Distant_Shotgun.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/shotgun/weap_shotgun_fire_plr_atmo_int1_01.wav"
+})
+sound.Add({
+    name = "Distant_ShotgunSUP.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    pitch = {80,110},
+    volume = 1,
+    sound = {"weapons/cod2019/shared/atmo/shotgun/weap_shotgunsup_fire_plr_atmo_int1_01.ogg",
+            "weapons/cod2019/shared/atmo/shotgun/weap_shotgunsup_fire_plr_atmo_int1_02.ogg",
+            "weapons/cod2019/shared/atmo/shotgun/weap_shotgunsup_fire_plr_atmo_int1_03.ogg",
+            "weapons/cod2019/shared/atmo/shotgun/weap_shotgunsup_fire_plr_atmo_int1_04.ogg",
+            "weapons/cod2019/shared/atmo/shotgun/weap_shotgunsup_fire_plr_atmo_int1_05.ogg",
+            "weapons/cod2019/shared/atmo/shotgun/weap_shotgunsup_fire_plr_atmo_int1_06.ogg"}
+})
+
+--- SMG ---
+sound.Add({
+    name = "Distant_SMG.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = "^weapons/cod2019/shared/atmo/smg/weap_smg_fire_plr_atmo_int1_01.wav"
+})
+
+--- Launchers ---
+--- JOKR ---
+sound.Add({
+    name = "Distant_JOKR.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    volume = 1,
+    pitch = {80,100},
+    sound = {
+        "weapons/cod2019/jokr/weap_juliet_launch_atmo_01.ogg",
+        }
+})
+sound.Add({
+    name = "Distant_JOKR.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    volume = 1,
+    pitch = {80,100},
+    sound = {
+        "weapons/cod2019/jokr/weap_juliet_launch_atmo_int_01.ogg",
+        }
+})
+--- PILA ---
+sound.Add({
+    name = "Distant_PILA.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    volume = 1,
+    pitch = {80,110},
+    sound =  "^weapons/cod2019/pila/weap_gromeo_fire_plr_atmo_01.ogg",
+
+})
+--- RPG ---
+sound.Add({
+    name = "Distant_RPG.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    volume = 1,
+    level = 140,
+    pitch = {80,110},
+    sound = "^weapons/cod2019/shared/atmo/launchers/weap_rpapa7_fire_plr_atmo_01.ogg"
+})
+--- STRELA ---
+sound.Add({
+    name = "Distant_Strela.Outside",
+    channel = ARC9.CHAN_DISTANT or 136,
+    level = 140,
+    volume = 1,
+    pitch = {85,110},
+    sound =
+        "^weapons/cod2019/strela/weap_kgolf_fire_plr_atmo_01.ogg",
+})
+
+sound.Add({
+    name = "Distant_Launcher.Inside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = {"weapons/cod2019/shared/atmo/launchers/weap_juliet_launch_atmo_int_01.ogg",
+            "weapons/cod2019/shared/atmo/launchers/weap_juliet_launch_atmo_int_02.ogg",
+            "weapons/cod2019/shared/atmo/launchers/weap_juliet_launch_atmo_int_03.ogg"}
+})
+
+sound.Add({
+    name = "Distant_Launcher.Outside",
+    channel = ARC9.CHAN_INDOORDISTANT or 144,
+    level = 140,
+    pitch = {80,110},
+    volume = 1,
+    sound = {"weapons/cod2019/shared/atmo/launchers/weap_mike203_fire_plr_atmos_01.ogg",
+            "weapons/cod2019/shared/atmo/launchers/weap_mike203_fire_plr_atmos_02.ogg",
+            "weapons/cod2019/shared/atmo/launchers/weap_mike203_fire_plr_atmos_03.ogg"}
+})
---
+++ lua/arc9/shared/sh_cod2019_sounds_layer.lua
@@ -0,0 +1,94 @@
+AddCSLuaFile()
+--- Reflections ---
+sound.Add({
+    name =           "Layer_AR.Outside",
+    channel =        ARC9.CHAN_LAYER + 4 or 137,
+    volume =         0.5,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/ar/weap_refl_ar_urb/weap_refl_ar_urb_ext_close_front_01.wav",
+})
+sound.Add({
+    name =           "Layer_AR.Inside",
+    channel =        ARC9.CHAN_INDOORLAYER or 6,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/ar/weap_refl_ar_urb/weap_refl_ar_urb_int_med_side_01.wav"
+})
+
+sound.Add({
+    name =           "Layer_ARSUP.Outside",
+    channel =        ARC9.CHAN_LAYER + 4 or 137,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/ar/weap_refl_ar_urb/weap_refl_arsup_urb_ext_dist_lr_01.wav"
+})
+sound.Add({
+    name =           "Layer_ARSUP.Inside",
+    channel =        ARC9.CHAN_INDOORLAYER or 6,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/ar/weap_refl_ar_urb/wex_arsup_urb_int_close_front_plr_01.wav",
+})
+
+sound.Add({
+    name =           "Layer_Pistol.Outside",
+    channel =        ARC9.CHAN_LAYER + 4 or 137,
+    volume =         0.75,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/pistol/weap_pist_ref_urb/weap_refl_pistol_urb_ext_dist_lr_01.wav"
+})
+sound.Add({
+    name =           "Layer_Pistol.Inside",
+    channel =        ARC9.CHAN_INDOORLAYER or 6,
+    volume =         0.75,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/pistol/weap_pist_ref_urb/weap_refl_pistol_urb_int_close_rear_01.wav"
+})
+
+sound.Add({
+    name =           "Layer_Shotgun.Outside",
+    channel =        ARC9.CHAN_LAYER + 4 or 137,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/shotgun/weap_refl_shotgun_urb_ext_dist_lr_01.wav"
+})
+sound.Add({
+    name =           "Layer_Shotgun.Inside",
+    channel =        ARC9.CHAN_INDOORLAYER or 6,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/shotgun/weap_refl_shotgun_urb_int_close_front_01.wav"
+})
+sound.Add({
+    name =           "Layer_ShotgunSUP.Inside",
+    channel =        ARC9.CHAN_INDOORLAYER or 6,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/shotgun/wex_shotgunsup_urb_int_close_front_plr_01.wav"
+})
+
+sound.Add({
+    name =           "Layer_Sniper.Outside",
+    channel =        ARC9.CHAN_LAYER + 4 or 137,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "^weapons/cod2019/shared/reflection/sniper/weap_refl_sniper_urb/weap_refl_sniper_urb_ext_dist_lr_01.wav"
+})
+sound.Add({
+    name =           "Layer_Sniper.Inside",
+    channel =        ARC9.CHAN_INDOORLAYER or 6,
+    volume =         1,
+    level =          140,
+    pitch =          {90,110},
+    sound =          "weapons/cod2019/shared/reflection/sniper/weap_refl_sniper_urb/weap_refl_sniper_urb_int_close_front_01.wav"
+})
