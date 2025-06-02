7289aac640186b590fc58c2f0a5631d6b1942eb0
TheOnly8Z <888888zombies@gmail.com>
2025-06-02 18:16:18 -0400

    gas cloud handles VJ npcs (they do blood differently sometimes)

--- lua/entities/arc9_cod2019_gas/init.lua
+++ lua/entities/arc9_cod2019_gas/init.lua
@@ -1,10 +1,9 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
-
 include("shared.lua")
 
 if CLIENT then
-    killicon.Add( "arc9_cod2019_gas", "vgui/killicons/cod2019_gas2.png", Color(251, 85, 25, 255))
+    killicon.Add("arc9_cod2019_gas", "vgui/killicons/cod2019_gas2.png", Color(251, 85, 25, 255))
 end
 
 ENT.smokeSound = nil
@@ -16,42 +15,35 @@ function ENT:Initialize()
     self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)]]
     self:AddFlags(FL_GRENADE)
     self:AddFlags(FL_ONFIRE)
-
     local repFil = RecipientFilter()
     repFil:AddAllPlayers()
-
     self.smokeSound = CreateSound(self, "MW19.GasLoop", repFil)
     self.smokeSound:Play()
-
     self:EmitSound("MW19.GasExplode")
-    
     ParticleEffectAttach("AC_nade_gas_dust", PATTACH_ABSORIGIN_FOLLOW, self, 0)
     ParticleEffect("AC_nade_gas_explode", self:GetPos(), self:GetAngles(), self, 0)
-
     self:SetParent(NULL)
     sound.EmitHint(SOUND_DANGER, self:GetPos(), self.GasRadius * 2, self.LifeTime, nil)
-end 
+end
 
 local function doesEntityBreathe(ent)
-    return ent:GetBloodColor() != BLOOD_COLOR_MECH && ent:GetBloodColor() != DONT_BLEED
+    return (ent:GetBloodColor() ~= BLOOD_COLOR_MECH and ent:GetBloodColor() ~= DONT_BLEED) or (ent.IsVJBaseSNPC and ent.BloodColor ~= false)
 end
 
 function ENT:Think()
-    if (CurTime() > self:GetCreationTime() + (self.LifeTime - 2)) then
+    if CurTime() > self:GetCreationTime() + (self.LifeTime - 2) then
         self:StopGas()
     end
-    
-    if (CurTime() - self:GetCreationTime() > self.LifeTime) then
+
+    if CurTime() - self:GetCreationTime() > self.LifeTime then
         self:Remove()
+
         return
     end
 
     for _, e in pairs(ents.FindInSphere(self:GetPos(), self.GasRadius)) do
-        if (e:IsPlayer() || e:IsNPC() || e:IsNextBot()) then
-            if (!doesEntityBreathe(e) || !e:IsLineOfSightClear(self:GetPos())) then
-                continue
-            end
-
+        if e:IsPlayer() or e:IsNPC() or e:IsNextBot() then
+            if not doesEntityBreathe(e) or not e:IsLineOfSightClear(self:GetPos()) then continue end
             local dmgInfo = DamageInfo()
             dmgInfo:SetAttacker(self:GetOwner())
             dmgInfo:SetDamage(9)
@@ -62,6 +54,7 @@ function ENT:Think()
     end
 
     self:NextThink(CurTime() + 0.25)
+
     return true
 end
 
@@ -72,13 +65,10 @@ end
 ENT.bStoppedGas = false
 
 function ENT:StopGas()
-    if (self.bStoppedGas) then
-        return
-    end
-
+    if self.bStoppedGas then return end
     self.bStoppedGas = true
 
-    if (self.smokeSound != nil) then
+    if self.smokeSound ~= nil then
         self.smokeSound:Stop()
     end
 
